56d395edc843436c40bf37e30e063825378ac69b
Mathieu Foucher <mfoucher@snowbird.com>
2025-09-21 20:18:10 -0600

    added datasets

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from scraper_forecaster import get_live_data
 from utils import format_time_column, plot_base_graph
 from plotly.subplots import make_subplots
 import os
+import json 
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 
 def main(option='PEAK'):
@@ -14,6 +17,15 @@ def main(option='PEAK'):
     df = get_live_data()
     df = format_time_column(df)
     
+    creds_info = json.loads(os.environ['GOOGLE_APP_CREDS'])
+    credentials = service_account.Credentials.from_service_account_info(creds_info)
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+    # Print off datasets (test):
+    for dataset in client.list_datasets():
+        st.write(dataset.dataset_id)
+
+
     # Prebuilt Forecasted Season from BigQuery:
     fdf = pd.read_csv('2526f_Streamlit.csv')
 
